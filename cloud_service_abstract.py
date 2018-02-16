87a3e967a9df85d701bcffc9244a690079ae25cb
MaxHausmann <sma.x@web.de>
2018-02-16 15:39:16 +0100

    Start project

---
+++ cloud_service_abstract.py
@@ -0,0 +1,24 @@
+from abc import ABC, abstractmethod
+
+
+class CloudService(ABC):
+
+    def __init__(self):
+        pass
+
+    def get_dirs(self):
+        pass
+
+    def get_chunk(self):
+        pass
+
+    def open(self):
+        pass
+
+    def connect(self):
+        pass
+
+    def create_dir(self):
+        pass
+
+
